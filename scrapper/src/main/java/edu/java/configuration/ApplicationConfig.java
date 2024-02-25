72187c4673a257ce7d816c8e95a8061489d6c776
chessplayer123 <ieriemkin20022002@gmail.com>
2024-02-26 00:00:47 +0300

    Register clients beans

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -9,7 +9,9 @@ import org.springframework.validation.annotation.Validated;
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
     @NotNull
-    Scheduler scheduler
+    Scheduler scheduler,
+    String githubUrl,
+    String stackoverflowUrl
 ) {
     public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
     }
--- scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
@@ -1,9 +1,21 @@
 package edu.java.configuration;
 
+import edu.java.client.github.repository.GHRepositoryClient;
+import edu.java.client.stackoverflow.question.SOQuestionClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
 public class ClientConfiguration {
+    @Bean
+    public GHRepositoryClient githubRepositoryClient(ApplicationConfig config) {
+        return new GHRepositoryClient(config.githubUrl());
+    }
+
+    @Bean
+    public SOQuestionClient stackOverflowQuestionClient(ApplicationConfig config) {
+        return new SOQuestionClient(config.stackoverflowUrl());
+    }
 }
